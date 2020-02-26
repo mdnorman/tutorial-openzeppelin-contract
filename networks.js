04e0bfd81cf30a48a9d92350b9eb54be3e83c912
Michael D. Norman <michael.norman@denormans.com>
2020-02-25 20:44:11 -0600

    added ganache network

--- networks.js
+++ networks.js
@@ -8,5 +8,13 @@ module.exports = {
       gasPrice: 5e9,
       networkId: '*',
     },
+    ganache: {
+      protocol: 'http',
+      host: 'localhost',
+      port: 7545,
+      gas: 5000000,
+      gasPrice: 5e9,
+      networkId: '*',
+    },
   },
 };
